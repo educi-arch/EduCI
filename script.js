d84e22301d9644ee1e04372e82ef4d4b15d1d863
dreyaifiora-lgtm <dreyaifiora@gmail.com>
2026-08-11 15:40:15 +0000

    Ajouter les fonctionnalités de la page d'accueil

---
+++ script.js
@@ -0,0 +1,9 @@
+// =========================================
+// EduCI - Fonctionnalités de la page d'accueil
+// =========================================
+
+const boutonCommencer = document.getElementById("commencer");
+
+boutonCommencer.addEventListener("click", function () {
+    alert("Bienvenue sur EduCI 🇨🇮📚 !");
+});
